41dfff8a32fe8295c31b71ae57d71ed88a261c82
Santi Arroyo <santi.arroyo551@gmail.com>
2022-05-12 13:54:29 -0400

    commit tree a1aec94e643765d0eeb2e7cef7713c435e2d1b2b

--- index.js
+++ index.js
@@ -1,1 +1,52 @@
 // Your code here
+const dodger = document.getElementById("dodger");
+
+function moveDodgerLeft() {
+    const test = dodger.style.bottom
+    console.log(test);
+    const leftNumbers = dodger.style.left.replace("px", "");
+    const left = parseInt(leftNumbers, 10);
+  
+    if (left > 0) {
+      dodger.style.left = `${left - 1}px`;
+    }
+  }
+
+  document.addEventListener("keydown", function (e) {
+    if (e.key === "ArrowLeft") {
+      moveDodgerLeft();
+    }
+  });
+
+
+
+
+function moveDodgerRight() {
+    const leftNumbers = dodger.style.left.replace("px", "");
+    const left = parseInt(leftNumbers, 10);
+  
+    if (left < 360) {
+      dodger.style.left = `${left + 10}px`;
+    }
+  }
+
+  document.addEventListener("keydown", function (e) {
+    if (e.key === "ArrowRight") {
+      moveDodgerRight();
+    }
+  });
+
+function moveDodgerUp () {
+    const upNumbers = dodger.style.bottom.replace("px", "");
+    const up = parseInt(upNumbers, 10);
+
+    if (up >= 0) {
+        dodger.style.bottom = `${up + 10}px`
+    }
+}
+document.addEventListener("keydown", function (e) {
+    if (e.key === "ArrowUp") {
+        moveDodgerUp();
+    }
+}) 
+
